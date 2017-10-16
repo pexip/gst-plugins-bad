8ab353801bd2389d0f5ba2e90429e27bf23ae5fb
Havard Graff <havard.graff@gmail.com>
2017-10-16 08:31:13 +0200

    openjpeg: libopenjp2.pc contains full include-path
    Hence it does not make sense to add "openjpeg-2.x" to the path.

--- ext/openjpeg/gstopenjpeg.h
+++ ext/openjpeg/gstopenjpeg.h
@@ -38,13 +38,7 @@
 #define OPJ_CPRL CPRL
 #else
 #include <stdio.h>
-# if defined(HAVE_OPENJPEG_2_2)
-#  include <openjpeg-2.2/openjpeg.h>
-# elif defined(HAVE_OPENJPEG_2_1)
-#  include <openjpeg-2.1/openjpeg.h>
-# else
-#  include <openjpeg-2.0/openjpeg.h>
-# endif
+#include <openjpeg.h>
 #endif
 
 #endif /* __GST_OPENJPEG_H__ */
