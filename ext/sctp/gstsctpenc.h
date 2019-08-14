a75be9ea30189991fcd547b5561d80d921e68ed5
John-Mark Bell <jmb@pexip.com>
2019-08-14 19:23:38 +0100

    SCTP: expose property to enable debug

--- ext/sctp/gstsctpenc.h
+++ ext/sctp/gstsctpenc.h
@@ -50,6 +50,7 @@ struct _GstSctpEnc
   guint32 sctp_association_id;
   guint16 remote_sctp_port;
   gboolean use_sock_stream;
+  gboolean debug_sctp;
 
   GstSctpAssociation *sctp_association;
   GstDataQueue *outbound_sctp_packet_queue;
